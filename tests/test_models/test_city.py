b906aff2b3e7130338c5121e3cb882a2628a4a04
Jukunye <jukunyes@gmail.com>
2023-10-15 14:54:50 +0300

    implemented unit test for city class

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,45 @@
+"""This module defines tests for State class"""
+import pycodestyle
+import unittest
+from models import city
+from models.city import City
+
+
+class TestState(unittest.TestCase):
+    """
+    TestState class defines unit tests for City class
+    """
+    def test_pycodestyle(self):
+        """Test that the code conforms to pycodestyle"""
+        style = pycodestyle.StyleGuide()
+        result = style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0, "pycodestyle error")
+
+    def test_docstring(self):
+        """Check for docstring presence"""
+        self.assertIsNotNone(city.__doc__)
+        self.assertIsNotNone(City.__doc__)
+
+    def test_initialization(self):
+        """Test with default value"""
+        c = City()
+        self.assertEqual(c.state_id, "")
+        self.assertEqual(c.name, "")
+        self.assertIsInstance(c.name, str)
+        self.assertIsInstance(c.state_id, str)
+
+    def test_attributes(self):
+        """
+        Test setting attributes of an instance and checking their values.
+        """
+        c = City()
+        c.state_id = "123xyz"
+        c.name = "Lagos"
+        self.assertEqual(c.state_id, "123xyz")
+        self.assertEqual(c.name, "Lagos")
+        self.assertIsInstance(c.name, str)
+        self.assertIsInstance(c.state_id, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
